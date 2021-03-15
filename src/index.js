9ee3192810cafeb9905fbc956e5b2ba4317d331e
Zhdmitry <zhoultko@gmail.com>
2021-03-15 21:49:17 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let str =n.toString();
+  let arr = str.split('');
+  let x = arr.reverse();
+    let y = x.join('');
+    if(y.includes("-") == true){
+        
+        y = y.substring(0, y.length - 1); 
+    }
+  return y;
 }
